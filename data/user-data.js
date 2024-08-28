0f2eafa3a7e4f2bc5bdc59bab753d6e5ae011aad
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-08-28 09:52:47 +0600

    project initialized

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'khandokarIsmailDev',
+  devUsername: "khandokarIsmailDev",
+  github: 'https://github.com/khandokarIsmailDev',
+  facebook: 'https://www.facebook.com/ismail.khandokar.315',
+  linkedIn: 'https://www.linkedin.com/in/khandokar-ismail-87a280301/',
+  twitter: 'https://twitter.com',
+  stackOverflow: 'https://stackoverflow.com',
+  leetcode: "https://leetcode.com/",
+  resume: "https://drive.google.com/file/d/1vzJRu70O0HPjdlPP3NzoYov6jsCA132c/view?usp=sharing",
+  skills: ['React', 'NextJS', 'Redux', 'Express','Docker', 'AWS', 'Prisma', 'MYSQL', 'Bootstrap', 'Wordpress','Jest'],
+  timezone: '+6'
+};
